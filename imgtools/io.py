0037a15c610b442e368b02faf4b6621a4b4a5b53
Michal Kazmierski <mkazmierski.poznan@gmail.com>
2020-01-17 16:51:50 -0500

    added docstrings to io functions

--- imgtools/io.py
+++ imgtools/io.py
@@ -1,18 +1,70 @@
+import os
+import numpy as np
 import SimpleITK as sitk
 from pydicom import dcmread
+from pydicom.misc import is_dicom
+
+from typing import Optional
 
 
-def read_dicom_series(path):
+def read_dicom_series(path: str,
+                      recursive: bool = False,
+                      series_id: Optional[str] = None) -> sitk.Image:
+    """Read DICOM series as SimpleITK Image.
+
+    Parameters
+    ----------
+    path
+       Path to directory containing the DICOM series.
+
+    recursive, optional
+       Whether to recursively parse the input directory when searching for
+       DICOM series,
+
+    series_id, optional
+       Specifies the DICOM series to load if multiple series are present in
+       the directory. If None and multiple series are present, loads the first
+       series found.
+
+
+    Returns
+    -------
+    The loaded image.
+
+    """
     reader = sitk.ImageSeriesReader()
-    dicom_names = reader.GetGDCMSeriesFileNames(path)
+    dicom_names = reader.GetGDCMSeriesFileNames(path,
+                                                seriesID=series_id if series_id else "",
+                                                recursive=recursive)
     reader.SetFileNames(dicom_names)
     return reader.Execute()
 
 
-def read_image(path):
-    return sitk.ReadImage(path)
+def find_dicom_paths(root_path: str, yield_directories: bool = False) -> str:
+    """Find DICOM file paths in the specified root directory file tree.
+
+    Parameters
+    ----------
+    root_path
+        Path to the root directory specifying the file hierarchy.
+
+    yield_directories, optional
+        Whether to yield paths to directories containing DICOM files as direct
+        children or separately to each file (default).
 
 
-def read_dicom_rtstruct(path):
-    return pydicom.dcmread(path)
+    Yields
+    ------
+    The paths to DICOM files or DICOM-containing directories (if
+    `yield_directories` is True).
 
+    """
+    for root, dirs, files in os.walk(root_path):
+        if yield_directories:
+            if any((is_dicom(os.path.join(root, f)) for f in files)):
+                yield root
+        else:
+            for f in files:
+                fpath = os.path.join(root, f)
+                if is_dicom(fpath):
+                    yield fpath
