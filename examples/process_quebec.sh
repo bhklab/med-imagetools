6bd69b2368444b2b25dad50e21925822bf68a188
Sejin Kim <sejinkim@uhnslurmbuildbox.uhnh4h.cluster>
2021-12-04 21:42:58 -0500

    example bash script with path parsing, fixed reference_frame
    Former-commit-id: a2084b0dffe014cf7a0eea285ea114b2e94729bc

--- examples/process_quebec.sh
+++ examples/process_quebec.sh
@@ -1,4 +1,8 @@
-python ../autopipeline.py\
-     ../data/Head-Neck-PET-CT \
-     ../data_nrrd/ \
+#!/usr/bin/env bash
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+TOP_DIR="$(dirname "$SCRIPT_DIR")"
+
+python $TOP_DIR/imgtools/autopipeline.py\
+     $SCRIPT_DIR/data/tcia_samples/Head-Neck-PET-CT \
+     $SCRIPT_DIR/data/tcia_nrrd \
      --modalities CT,RTSTRUCT
