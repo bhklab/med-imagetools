cd6b5bf8638d6093c5cf1c205771639a0ed80c7b
Sejin Kim <hello@sejin.kim>
2021-12-09 21:22:18 -0500

    set roi_names to avoid memoryerror

--- tests/test_modalities.py
+++ tests/test_modalities.py
@@ -21,18 +21,18 @@ from imgtools.pipeline import Pipeline
 @pytest.fixture
 def modalities_path():
     curr_path = pathlib.Path(__file__).parent.parent.resolve()
-    qc_path = pathlib.Path(os.path.join(curr_path, "data", "Head-Neck-PET-CT", "HN-CHUS-082"))
-    path = {}
-    path["CT"] = os.path.join(qc_path, "08-27-1885-CA ORL FDG TEP-06980/3.000000-Merged-05195")
-    path["RTSTRUCT"] = os.path.join(qc_path, "08-27-1885-06980/Pinnacle POI-67882")
-    path["RTDOSE"] = os.path.join(qc_path, "08-27-1885-06980/89632")
-    path["PT"] = os.path.join(qc_path, "08-27-1885-TEP cancerologique TEP-06980/552650.000000-LOR-RAMLA-72508")
-    # qc_path = pathlib.Path(os.path.join(curr_path, "data", "Head-Neck-PET-CT", "HN-CHUS-052"))
+    # qc_path = pathlib.Path(os.path.join(curr_path, "data", "Head-Neck-PET-CT", "HN-CHUS-082"))
     # path = {}
-    # path["CT"] = os.path.join(qc_path, "08-27-1885-CA ORL FDG TEP POS TX-94629/3.000000-Merged-06362")
-    # path["RTSTRUCT"] = os.path.join(qc_path, "08-27-1885-OrophCB.0OrophCBTRTID derived StudyInstanceUID.-94629/Pinnacle POI-41418")
-    # path["RTDOSE"] = os.path.join(qc_path, "08-27-1885-OrophCB.0OrophCBTRTID derived StudyInstanceUID.-94629/11376")
-    # path["PT"] = os.path.join(qc_path, "08-27-1885-CA ORL FDG TEP POS TX-94629/532790.000000-LOR-RAMLA-44600")
+    # path["CT"] = os.path.join(qc_path, "08-27-1885-CA ORL FDG TEP-06980/3.000000-Merged-05195")
+    # path["RTSTRUCT"] = os.path.join(qc_path, "08-27-1885-06980/Pinnacle POI-67882")
+    # path["RTDOSE"] = os.path.join(qc_path, "08-27-1885-06980/89632")
+    # path["PT"] = os.path.join(qc_path, "08-27-1885-TEP cancerologique TEP-06980/552650.000000-LOR-RAMLA-72508")
+    qc_path = pathlib.Path(os.path.join(curr_path, "data", "Head-Neck-PET-CT", "HN-CHUS-052"))
+    path = {}
+    path["CT"] = os.path.join(qc_path, "08-27-1885-CA ORL FDG TEP POS TX-94629/3.000000-Merged-06362")
+    path["RTSTRUCT"] = os.path.join(qc_path, "08-27-1885-OrophCB.0OrophCBTRTID derived StudyInstanceUID.-94629/Pinnacle POI-41418")
+    path["RTDOSE"] = os.path.join(qc_path, "08-27-1885-OrophCB.0OrophCBTRTID derived StudyInstanceUID.-94629/11376")
+    path["PT"] = os.path.join(qc_path, "08-27-1885-CA ORL FDG TEP POS TX-94629/532790.000000-LOR-RAMLA-44600")
     return path
 
 @pytest.mark.parametrize("modalities", ["CT", "RTSTRUCT", "RTDOSE", "PT"])
@@ -58,7 +58,7 @@ def test_modalities(modalities, modalities_path):
     else:
         img = read_dicom_auto(path["CT"])
         struc = read_dicom_auto(path[modalities])
-        make_binary_mask = StructureSetToSegmentation(roi_names=[], continuous=False)
+        make_binary_mask = StructureSetToSegmentation(roi_names=['GTV'], continuous=False)
         mask = make_binary_mask(struc, img)
         A = sitk.GetArrayFromImage(mask)
         assert len(A.shape)==4
