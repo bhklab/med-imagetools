0cd70697c1550ddf23b48ec1bb1944706bdd96f8
Sejin Kim <40668167+skim2257@users.noreply.github.com>
2022-10-22 17:09:17 +0000

    Added use case 1 (scratch vs precrawl w --update)

---
+++ use-cases/use_case_1_from_scratch.sh
@@ -0,0 +1,7 @@
+conda activate med-imagetools
+
+autopipeline \
+    INPUT_DIR \
+    OUTPUT_DIR \
+    --modalities CT,RTSTRUCT \
+    --update
---
+++ use-cases/use_case_1_pre_crawled.sh
@@ -0,0 +1,6 @@
+conda activate med-imagetools
+
+autopipeline \
+    INPUT_DIR \
+    OUTPUT_DIR \
+    --modalities CT,RTSTRUCT
