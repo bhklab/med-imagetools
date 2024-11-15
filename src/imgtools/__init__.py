c0d08b1b3ccbf3d34f569b2e7590ceb8fc09e91d
Jermiah Joseph <jermiahjoseph98@gmail.com>
2024-11-14 19:18:36 -0500

    chore: update authors and maintainers in pyproject.toml,  and add __version__ in __init__.py

--- src/imgtools/__init__.py
+++ src/imgtools/__init__.py
@@ -1,4 +1,8 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+||||||| parent of 085feaf (chore: update authors and maintainers in pyproject.toml,  and add __version__ in __init__.py)
+=======
+>>>>>>> 085feaf (chore: update authors and maintainers in pyproject.toml,  and add __version__ in __init__.py)
 from . import io, ops, utils, pipeline
 
 __all__ = ["io", "ops", "utils", "pipeline"]
