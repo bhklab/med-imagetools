2af1854b69ba2c6938801b431dd45e94721e1f47
Jermiah Joseph <44614774+jjjermiah@users.noreply.github.com>
2025-01-15 11:22:56 -0500

    feat: Add image utilities and geometry class, update ruff configuration (#168)

--- src/imgtools/__init__.py
+++ src/imgtools/__init__.py
@@ -1,1 +1,12 @@
-__version__ = "1.12.0"
+__version__ = '1.12.0'
+
+from .dicom import find_dicoms, lookup_tag, similar_tags, tag_exists
+from .logging import logger
+
+__all__ = [
+    'find_dicoms',
+    'lookup_tag',
+    'similar_tags',
+    'tag_exists',
+    'logger',
+]
