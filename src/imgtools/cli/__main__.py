aa44ac438ac84c70e1a45bcfe94e66846b3ffb11
Jermiah <jermiahjoseph98@gmail.com>
2025-01-15 23:10:10 +0000

    feat(cli): Add index command to crawl and index DICOM images in a directory

--- src/imgtools/cli/__main__.py
+++ src/imgtools/cli/__main__.py
@@ -4,29 +4,31 @@ from imgtools import __version__
 from imgtools.cli import set_log_verbosity
 from imgtools.cli.dicomfind import find_dicoms
 from imgtools.cli.dicomsort import dicomsort
+from imgtools.cli.index import index
 
 
 @click.group(
-	no_args_is_help=True,
+    no_args_is_help=True,
 )
 @set_log_verbosity()
 @click.version_option(
-	version=__version__,
-	package_name='med-imagetools',
-	prog_name='imgtools',
-	message='%(package)s:%(prog)s:%(version)s',
+    version=__version__,
+    package_name="med-imagetools",
+    prog_name="imgtools",
+    message="%(package)s:%(prog)s:%(version)s",
 )
 @click.help_option(
-	'-h',
-	'--help',
+    "-h",
+    "--help",
 )
 def cli(verbose: int, quiet: bool) -> None:
-	"""A collection of tools for working with medical imaging data."""
-	pass
+    """A collection of tools for working with medical imaging data."""
+    pass
 
 
+cli.add_command(index)
 cli.add_command(dicomsort)
-cli.add_command(find_dicoms, 'find-dicoms')
+cli.add_command(find_dicoms, "find-dicoms")
 
-if __name__ == '__main__':
-	cli()
+if __name__ == "__main__":
+    cli()
