ce73a6dcbf8222e5ade38a0fc78820daab1c8d3a
Jermiah <jermiahjoseph98@gmail.com>
2025-01-30 17:12:22 +0000

    refactor(helper_types): update data classes and improve Vector3D initialization

--- src/imgtools/coretypes/helper_types.py
+++ src/imgtools/coretypes/helper_types.py
@@ -67,17 +67,17 @@ Matrix3DFlat: TypeAlias = Tuple[
 FlattenedMatrix = Matrix3DFlat
 
 
-@dataclass(frozen=True)
-class ImageGeometry:
-    """Represents the geometry of a 3D image."""
+# @dataclass(frozen=True)
+# class ImageGeometry:
+#     """Represents the geometry of a 3D image."""
 
-    size: Size3D
-    origin: Point3D
-    direction: Direction
-    spacing: Spacing
+#     size: Size3D
+#     origin: Point3D
+#     direction: Direction
+#     spacing: Spacing
 
 
-@dataclass(frozen=True)
+@dataclass
 class Vector3D:
     """
     Represent a vector in 3D space.
@@ -105,16 +105,30 @@ class Vector3D:
     y: float
     z: float
 
+    def __init__(self, *args: float) -> None:
+        """Initialize a Vector3D with x, y, z components."""
+        match args:
+            case [x, y, z]:
+                self.x, self.y, self.z = x, y, z
+            case [tuple_points] if isinstance(tuple_points, tuple):
+                self.x, self.y, self.z = tuple_points
+            case _:
+                errmsg = (
+                    f"{self.__class__.__name__} expects 3 values for x, y, z."
+                    f" Got {len(args)} values for {args}."
+                )
+                raise ValueError(errmsg)
+
     def __iter__(self) -> Iterator[float]:
         """Allow iteration over the components."""
         return iter((self.x, self.y, self.z))
 
     def __repr__(self) -> str:
         """Return a string representation of the Vector3D."""
-        return f"Vector3D(x={self.x:.3f}, y={self.y:.3f}, z={self.z:.3f})"
+        cls = self.__class__.__name__
+        return f"{cls}(x={self.x:.1f}, y={self.y:.1f}, z={self.z:.1f})"
 
 
-@dataclass(frozen=True)
 class Spacing(Vector3D):
     """
     Represent the spacing in 3D space.
@@ -125,8 +139,7 @@ class Spacing(Vector3D):
     # inherits everything from Vector3D
 
 
-@dataclass(frozen=True)
-class Point3D(Vector3D):
+class Coordinate3D(Vector3D):
     """
     Represent a point in 3D space.
     Inherits from Vector3D.
@@ -134,24 +147,47 @@ class Point3D(Vector3D):
     Can add and subtract other Point3D or Size3D objects.
     """
 
-    def __add__(self, other: Point3D | Size3D) -> Point3D:
+    def __add__(self, other: Coordinate3D | Size3D) -> Coordinate3D:
         """Add another Vector3D to this vector."""
         match other:
-            case Point3D(x, y, z):
-                return Point3D(self.x + x, self.y + y, self.z + z)
+            case Coordinate3D(x, y, z):
+                return Coordinate3D(self.x + x, self.y + y, self.z + z)
             case Size3D(width, height, depth):
-                return Point3D(self.x + width, self.y + height, self.z + depth)
+                return Coordinate3D(
+                    self.x + width, self.y + height, self.z + depth
+                )
+            case tuple() if len(other) == 3:
+                return Coordinate3D(
+                    self.x + other[0], self.y + other[1], self.z + other[2]
+                )
+            case _:
+                errmsg = (
+                    f"Unsupported type for addition: {type(other)}."
+                    " Expected Coordinate3D or Size3D."
+                )
 
-    def __sub__(self, other: Point3D | Size3D) -> Point3D:
+    def __sub__(self, other: Coordinate3D | Size3D) -> Coordinate3D:
         """Subtract another Vector3D from this vector."""
         match other:
-            case Point3D(x, y, z):
-                return Point3D(self.x - x, self.y - y, self.z - z)
+            case Coordinate3D(x, y, z):
+                return Coordinate3D(self.x - x, self.y - y, self.z - z)
             case Size3D(width, height, depth):
-                return Point3D(self.x - width, self.y - height, self.z - depth)
+                return Coordinate3D(
+                    self.x - width, self.y - height, self.z - depth
+                )
+            case tuple() if len(other) == 3:
+                return Coordinate3D(
+                    self.x - other[0], self.y - other[1], self.z - other[2]
+                )
+            case _:
+                errmsg = (
+                    f"Unsupported type for subtraction: {type(other)}."
+                    " Expected Point3D or Size3D."
+                )
+                raise TypeError(errmsg)
 
 
-@dataclass(frozen=True)
+@dataclass
 class Size3D:
     """
     Represent the size (width, height, depth) of a 3D object.
@@ -175,6 +211,18 @@ class Size3D:
     height: float
     depth: float
 
+    def __init__(self, *args: float) -> None:
+        match args:
+            case [width, height, depth]:
+                self.width, self.height, self.depth = width, height, depth
+            case [tuple_points] if isinstance(tuple_points, tuple):
+                self.width, self.height, self.depth = tuple_points
+            case _:
+                errmsg = (
+                    f"{self.__class__.__name__} expects 3 values for width, height, depth."
+                    f" Got {len(args)} values for {args}."
+                )
+
     @property
     def volume(self) -> float:
         """Calculate the volume of the 3D object."""
@@ -187,9 +235,20 @@ class Size3D:
     def __repr__(self) -> str:
         """Return a string representation of the Size3D."""
         return (
-            f"Size3D(w={self.width:.3f},"
-            " h={self.height:.3f}, d={self.depth:.3f})"
+            f"Size3D(w={self.width:.1f},"
+            f" h={self.height:.1f}, d={self.depth:.1f})"
         )
+
+
+"""
+jermiah (01/30/2025): 
+not sure about this direction class, was an initial proof-of-concept
+doesnt seem like a dealbreaker to need, havent dealt with any issues
+Really only wanted to use it for docs/tutorials to show how images
+are oriented + work via directions for sitk.Image
+
+lets not consider it too much unless we need to debug with it
+"""
 
 
 @dataclass(frozen=True, eq=True)
@@ -292,51 +351,82 @@ if __name__ == "__main__":
     from pathlib import Path
     from rich import print
 
+    ########################################
+    # Vector3D
+    ########################################
+
+    vector1 = Vector3D(1.0, 2.0, 3.0)
+
+    vector2 = Vector3D((1.0, 2.0, 3.0))  # as tuple input
+    assert all((attr1 == attr2) for attr1, attr2 in zip(vector1, vector2))
+
+    for attr in vector1:
+        print(attr)
+
+    print(vector1)
+
+    ########################################
+    # Coordinate3D
+    ########################################
+
+    point = Coordinate3D(10.0, 20.0, 30.0)
+    point2 = Coordinate3D((15.0, 25.0, 35.0))
+
+    print(point)
+    print(point2)
+
+    size_tuple = (50.0, 60.0, 70.0)
+    size = Size3D(size_tuple)
+    point_plus_size = point + size_tuple
+
+    print(f"Adding {point=} and {size_tuple=} = {point_plus_size}")
+    print(f"Adding {point=} and {size=} = {point + size}")
+
     # Create instances of each class
-    point = Point3D(10.0, 20.0, 30.0)
-    size = Size3D(50.0, 60.0, 70.0)
-    direction = Direction.from_matrix(
-        (
-            (0.707, 0.707, 0.0),
-            (-0.707, 0.707, 0.0),
-            (0.0, 0.0, 1.0),
-        )
-    )
+    # point = Point3D(10.0, 20.0, 30.0)
+    # size = Size3D(50.0, 60.0, 70.0)
+    # direction = Direction.from_matrix(
+    #     (
+    #         (0.707, 0.707, 0.0),
+    #         (-0.707, 0.707, 0.0),
+    #         (0.0, 0.0, 1.0),
+    #     )
+    # )
 
-    # Testing Point3D and Size3D operations
-    new_point = point + size
+    # # Testing Point3D and Size3D operations
+    # new_point = point + size
 
-    # Printing out the details
-    print(f"Point: {point}")
-    print(f"Size: {size}")
-    print(f"New point after adding size: {new_point}")
-    print(f"Direction: {direction}")
+    # # Printing out the details
+    # print(f"Point: {point}")
+    # print(f"Size: {size}")
+    # print(f"New point after adding size: {new_point}")
+    # print(f"Direction: {direction}")
 
-    # Unpacking the values
-    print("Unpacked Point:", tuple(point))  # (10.0, 20.0, 30.0)
-    print("Unpacked Size:", tuple(size))  # (50.0, 60.0, 70.0)
+    # # Unpacking the values
+    # print("Unpacked Point:", tuple(point))  # (10.0, 20.0, 30.0)
+    # print("Unpacked Size:", tuple(size))  # (50.0, 60.0, 70.0)
 
-    example_image = np.random.rand(10, 10, 10)
-    example_sitk_image = sitk.GetImageFromArray(example_image)
+    # example_image = np.random.rand(10, 10, 10)
+    # example_sitk_image = sitk.GetImageFromArray(example_image)
 
-    # Create a direction vector
-    # 3x3 Direction Matrix
-    direction_3d = Direction.from_matrix(
-        (
-            (0.707, 0.707, 0.0),
-            (-0.707, 0.707, 0.0),
-            (0.0, 0.0, 1.0),
-        )
-    )
+    # # Create a direction vector
+    # # 3x3 Direction Matrix
+    # direction_3d = Direction.from_matrix(
+    #     (
+    #         (0.707, 0.707, 0.0),
+    #         (-0.707, 0.707, 0.0),
+    #         (0.0, 0.0, 1.0),
+    #     )
+    # )
 
-    print(f"{direction_3d=}")
-    example_sitk_image.SetDirection(direction_3d)
+    # print(f"{direction_3d=}")
+    # example_sitk_image.SetDirection(direction_3d)
 
-    # make another direction from a flattened
-    direction_3d_flat = Direction(
-        matrix=(0.707, 0.707, 0.0, -0.707, 0.707, 0.0, 0.0, 0.0, 1.0)
-    )
-    print(f"{direction_3d_flat=}")
-    print(f"{(direction_3d_flat==direction_3d)=}")
+    # # make another direction from a flattened
+    # direction_3d_flat = Direction(
+    #     matrix=(0.707, 0.707, 0.0, -0.707, 0.707, 0.0, 0.0, 0.0, 1.0)
+    # )
+    # print(f"{direction_3d_flat=}")
+    # print(f"{(direction_3d_flat==direction_3d)=}")
 
-    print(f"{example_sitk_image=}")
+    # print(f"{example_sitk_image=}")
