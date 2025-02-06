a8825741006650fae3f36fd5bbe270768b87965a
Jermiah Joseph <44614774+jjjermiah@users.noreply.github.com>
2025-02-06 09:52:14 -0500

    feat: Add examples module for image dataset tools (#212)
    - **New Features**
    - Introduced new functions for streamlined access to example dataset
    resources, mapping key identifiers to their corresponding file paths and
    images.
    - Added a dynamic capability to conditionally include functionalities
    based on the availability of test data.
    - **Bug Fixes**
    	- Enhanced error handling for missing dataset files.
    - **Tests**
    - Added a new test function to verify the integrity and presence of
    example dataset images.

--- src/imgtools/__init__.py
+++ src/imgtools/__init__.py
@@ -1,6 +1,7 @@
 __version__ = "1.19.0"
 
 from .coretypes import Coordinate3D, Size3D, Spacing3D, Vector3D
+from .datasets import example_data, example_data_paths
 from .dicom import find_dicoms, lookup_tag, similar_tags, tag_exists
 from .logging import logger
 
@@ -15,4 +16,7 @@ __all__ = [
     "Size3D",
     "Spacing3D",
     "Vector3D",
+    ## example data
+    "example_data",
+    "example_data_paths",
 ]
