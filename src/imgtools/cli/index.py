0234fe0cbc2de192ca31e7f378b718c016d2548d
Jermiah Joseph <jermiahjoseph98@gmail.com>
2025-02-11 15:10:56 +0000

    feat: add index command to crawl and index DICOM images

---
+++ src/imgtools/cli/index.py
@@ -0,0 +1,47 @@
+import os
+import pathlib
+
+import click
+
+from imgtools.crawler import crawl
+from imgtools.logging import logger
+
+DEFAULT_WORKERS = os.cpu_count() - 2
+
+
+@click.command()
+@click.argument(
+    "source_directory",
+    type=click.Path(
+        exists=True,
+        file_okay=False,
+        dir_okay=True,
+        readable=True,
+        path_type=pathlib.Path,
+        resolve_path=True,
+    ),
+)
+@click.option(
+    "-j",
+    "--num-workers",
+    type=int,
+    default=DEFAULT_WORKERS,
+    show_default=True,
+    help="Number of worker processes to use for multiprocessing.",
+)
+@click.help_option(
+    "-h",
+    "--help",
+)
+def index(
+    source_directory: pathlib.Path,
+    num_workers: int,
+) -> None:
+    """Crawl a directory and index dicoms."""
+
+    logger.info(f"Indexing images in {source_directory}.")
+
+    # TODO: extend parameters such as csv_path, json_path, etc.
+    crawl(top=source_directory, n_jobs=num_workers)
+
+    logger.info("Indexing complete.")
