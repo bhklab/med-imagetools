c9d3b4f94e76154c6ea91d3a2f3eefd20554f5d3
Jermiah Joseph <jermiahjoseph98@gmail.com>
2025-02-11 14:46:05 +0000

    fix: removed import

--- src/imgtools/modules/__init__.py
+++ src/imgtools/modules/__init__.py
@@ -5,13 +5,6 @@ from .scan import Scan
 from .segmentation import Segmentation, accepts_segmentations, map_over_labels
 from .sparsemask import SparseMask
 from .structureset import StructureSet
-from .structureset_helpers import (
-    DicomInput,
-    extract_roi_meta,
-    extract_roi_names,
-    load_rtstruct_dcm,
-    rtstruct_reference_uids,
-)
 
 __all__ = [
     "Segmentation",
@@ -23,10 +16,4 @@ __all__ = [
     "DataGraph",
     "SparseMask",
     "Scan",
-    # structureset_helpers
-    "DicomInput",
-    "extract_roi_meta",
-    "extract_roi_names",
-    "load_rtstruct_dcm",
-    "rtstruct_reference_uids",
 ]
