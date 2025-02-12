e7dec7d00dfad2a628dc3c1bde61f8328d8b4c58
Jermiah Joseph <jermiahjoseph98@gmail.com>
2025-02-12 17:54:36 +0000

    feat: enhance Scan class to include comprehensive image metadata retrieval

--- src/imgtools/modules/scan.py
+++ src/imgtools/modules/scan.py
@@ -9,3 +9,34 @@ class Scan:
     def __init__(self, image: sitk.Image, metadata: Dict[str, T]) -> None:
         self.image = image
         self.metadata = metadata
+
+        self.metadata.update(self._getimgmetadata())
+
+    def _getimgmetadata(self):
+        size = self.image.GetSize()
+        spacing = self.image.GetSpacing()
+        origin = self.image.GetOrigin()
+        direction = self.image.GetDirection()
+
+        statisticsfilter = sitk.StatisticsImageFilter()
+
+        statisticsfilter.Execute(self.image)
+
+        return {
+            "size": size,
+            "spacing": spacing,
+            "origin": origin,
+            "direction": direction,
+            "min": statisticsfilter.GetMinimum(),
+            "max": statisticsfilter.GetMaximum(),
+            "mean": statisticsfilter.GetMean(),
+            "std": statisticsfilter.GetSigma(),
+        }
+
+    def __repr__(self) -> str:  # type: ignore
+        # return f"Scan(metadata={self.metadata})"
+        metadata_str = "\n\t".join(
+            [f"{k}: {v}" for k, v in self.metadata.items()]
+        )
+
+        return f"Scan(\n\t{metadata_str}\n)"
