460f25c65463f4d68c9b61a20249c29811590de9
semantic-release <semantic-release>
2025-02-14 20:49:42 +0000

    chore(sem-ver): 2.0.0-rc.6

--- src/imgtools/__init__.py
+++ src/imgtools/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "2.0.0-rc.5"
+__version__ = "2.0.0-rc.6"
 
 from .coretypes import BoxPadMethod, Coordinate3D, RegionBox, Size3D, Spacing3D
 from .datasets import example_data, example_data_paths
