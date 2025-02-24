8604299c242226837c87a6e04b903fa2eb863ff7
Katy Scott <k.l.scott16@gmail.com>
2025-02-24 11:34:10 -0500

    tests: wrote tests for dimension functions added
    Trying to write test functions for the original components was difficult, so just wrote tests for the new getter functions

--- tests/modules/test_segmentation.py
+++ tests/modules/test_segmentation.py
@@ -3,123 +3,104 @@ import pytest
 import SimpleITK as sitk
 from pathlib import Path
 from imgtools.modules import Segmentation, StructureSet
-from imgtools.io import read_dicom_rtstruct, read_dicom_series
-from imgtools.ops import StructureSetToSegmentation
-
-
-@pytest.fixture
-def HNPCT_data(data_paths) -> dict[str, Path | str]:
-    dataset_key = 'Head-Neck-PET-CT'
-    parent_dir_path = data_paths[dataset_key]
-    image_path = parent_dir_path / "HN-CHUS-052/08-27-1885-CA ORL FDG TEP POS TX-94629/3.000000-Merged-06362/"
-    mask_path = parent_dir_path / "HN-CHUS-052/08-27-1885-OrophCB.0OrophCBTRTID derived StudyInstanceUID.-94629/Pinnacle POI-41418/1-1.dcm"
-    return {"image": image_path, "mask": mask_path, "roi_name": "GTV1"}
+from imgtools.io import read_dicom_series
+from imgtools.coretypes import RegionBox, Coordinate3D, Size3D
 
 @pytest.fixture
-def image_CT(HNPCT_data) -> sitk.Image:
-    image = read_dicom_series(HNPCT_data["image"].resolve())
-    return image
+def HNPCT_paths():
+    return {
+        "base_image_path": Path("Head-Neck-PET-CT/HN-CHUS-052/08-27-1885-CA ORL FDG TEP POS TX-94629/3.000000-Merged-06362"),
+        "mask_path": Path("Head-Neck-PET-CT/HN-CHUS-052/08-27-1885-OrophCB.0OrophCBTRTID derived StudyInstanceUID.-94629/Pinnacle POI-41418/1-1.dcm")
+    }
 
 @pytest.fixture
-def mask_RT(HNPCT_data, image_CT) -> sitk.Image:
-    image_data = HNPCT_data
-    reference_image = image_CT
+def HNPCT_roi_name():
+    return "GTV1"
 
+@pytest.fixture
+def NSCLC_Radiomics():
+    return {
+        "dataset_name": "NSCLC-Radiomics",
+        "base_image_path": Path("NSCLC-Radiomics/LUNG1-001/1.3.6.1.4.1.32722.99.99.298991776521342375010861296712563382046"),
+        "mask_path": Path("NSCLC-Radiomics/LUNG1-001/1.3.6.1.4.1.32722.99.99.227938121586608072508444156170535578236/00000001.dcm")
+    }
+
+@pytest.fixture
+def NSCLC_Radiomics_roi_name():
+    return "GTV-1"
+
+@pytest.fixture
+def image_data_paths(data_paths, NSCLC_Radiomics) -> dict[str, Path]:
+    parent_dir_path = data_paths[NSCLC_Radiomics["dataset_name"]].parent
+    image_path = parent_dir_path / NSCLC_Radiomics['base_image_path']
+    mask_path = parent_dir_path / NSCLC_Radiomics['mask_path']
+    return {"image_path": image_path, "mask_path": mask_path}
+    
+
+@pytest.fixture
+def image_CT(image_data_paths) -> sitk.Image:
+    return read_dicom_series(image_data_paths["image_path"].resolve())
+
+@pytest.fixture
+def structure_set_RT(image_data_paths) -> StructureSet:
     # Load just the GTV ROI into a StructureSet object
-    rt_structure_set = StructureSet.from_dicom_rtstruct(image_data['mask'], roi_name_pattern=image_data['roi_name'])
+    return StructureSet.from_dicom_rtstruct(image_data_paths['mask_path'])
 
-    # Initialize a mask with the same size as the reference image
-    size = reference_image.GetSize()[::-1] + (1,)
-    mask = np.zeros(size, dtype=np.uint8)
-
-    # Convert StructureSet points to a binary mask
-    rt_structure_set.get_mask(reference_image = reference_image, 
-                              mask = mask, 
-                              label = 1, 
-                              idx = 1, 
-                              continuous = False)
-
-    # Convert the mask from an nd.array to a SimpleITK Image
-    mask = sitk.GetImageFromArray(mask)
-    # Copy metadata from the reference image to the mask
-    mask.CopyInformation(reference_image)
-
-    return mask
+@pytest.fixture
+def segmentation_RT(structure_set_RT, image_CT) -> Segmentation:
+    # Convert the StructureSet object to a Segmentation object
+    return structure_set_RT.to_segmentation(reference_image=image_CT, 
+                                            roi_names = structure_set_RT.roi_names, 
+                                            continuous = False)
+    
 
 
-
-def test_base_segmentation_construction(mask_RT):
-    "Test default Segmentation construction"
-    segmentation = Segmentation(mask_RT)
-
-    assert segmentation.raw_roi_names == {}
-    assert segmentation.metadata == {}
-    assert segmentation.num_labels == 1
-    assert segmentation.frame_groups is None
-
-    assert len(segmentation.roi_indices) == 1
-    assert 'label_1' in segmentation.roi_indices
-    assert segmentation.roi_indices['label_1'] == 1
+def test_get_label_from_name(segmentation_RT, NSCLC_Radiomics_roi_name):
+    actual_label = segmentation_RT.get_label_from_name(name = NSCLC_Radiomics_roi_name)
+    assert actual_label == 1
 
 
-# def test_segmentation_construction_with_roi_indices():
-#     "Test Segmentation construction with roi_indices"
-#     mask = example_data()['mask']
-#     segmentation = Segmentation(mask, roi_indices={'star': 1})
+@pytest.mark.parametrize(
+    "label, name",
+    [
+       (1, None),
+       (None, "GTV-1")
+    ]
+)
+def test_get_label(segmentation_RT, label, name, image_CT):
+    actual_image = segmentation_RT.get_label(label, name)
 
-#     assert segmentation.raw_roi_names == {}
-#     assert segmentation.metadata == {}
-#     assert segmentation.num_labels == 1
-
-#     assert len(segmentation.roi_indices) == 1
-#     assert 'star' in segmentation.roi_indices
-#     assert segmentation.roi_indices['star'] == 1
-
-
-# def test_segmentation_construction_with_existing_roi_indices():
-#     "Test Segmentation construction with roi_indices"
-#     mask = example_data()['mask']
-#     segmentation = Segmentation(mask, roi_indices={'star': 1}, existing_roi_indices={'background': 0})
-
-#     assert segmentation.raw_roi_names == {}
-#     assert segmentation.metadata == {}
-#     assert segmentation.num_labels == 1
-
-#     assert len(segmentation.roi_indices) == 1
-#     assert 'star' in segmentation.roi_indices
-#     assert segmentation.roi_indices['star'] == 1
-
-#     assert len(segmentation.roi_indices) == 1
-#     assert 'background' in segmentation.roi_indices
-#     assert segmentation.roi_indices['background'] == 0
+    assert actual_image.GetSize() == image_CT.GetSize()
+    assert isinstance(actual_image, sitk.Image)
+    assert actual_image.GetSpacing() == image_CT.GetSpacing()
+    assert actual_image.GetOrigin() == image_CT.GetOrigin()
 
 
-# @pytest.fixture
-# def segmentation():
-#     # Segmentation object
-#     mask = example_data()['mask']
-#     return Segmentation.from_dicom(mask, roi_indices={'star': 1}, existing_roi_indices={'background': 0})
 
-# def star_mask(segmentation):
-#     # SimpleITK Image of the star mask
-#     return segmentation[0]
-
-
-# def test_get_label_from_name(segmentation, name = 'star'):
-#     actual_label = segmentation.get_label_from_name(name = name)
-#     assert actual_label == 1
-
-# @pytest.mark.parameterize(
-#     "label, name",
-#     [
-#         1, None,
-#         None, "star"
-#     ]
-# )
-# def test_get_label(segmentation, label, name):
-#     actual_image = segmentation.get_label(label = label, name = name)
-
-#     pass
+def test_compute_statistics(segmentation_RT):
+    label_image = segmentation_RT.get_label(name = "GTV-1")
+    stats = segmentation_RT.compute_statistics(label_image)
+    assert isinstance(stats, sitk.LabelShapeStatisticsImageFilter)
+    print(stats.GetNumberOfLabels())
+    assert stats.GetNumberOfLabels() == 1
+    assert stats.GetNumberOfPixels(label=1) == 56271
 
 
+def test_get_label_bounding_box(segmentation_RT):
+    reg_box = segmentation_RT.get_label_bounding_box(name = "GTV-1")
+
+    assert isinstance(reg_box, RegionBox)
+    assert reg_box.min == Coordinate3D(290, 226, 65)
+    assert reg_box.max == Coordinate3D(388, 317, 86)
+    assert reg_box.size == Size3D(98, 91, 21)
+
+
+def test_get_label_dimensions(segmentation_RT):
+    dims = segmentation_RT.get_label_dimensions(name = "GTV-1")
+    assert dims == Size3D(98, 91, 21)
+
+
+def test_get_label_number_of_pixels(segmentation_RT):
+    num_pixels = segmentation_RT.get_label_number_of_pixels(name = "GTV-1")
+    assert num_pixels == 56271
 
