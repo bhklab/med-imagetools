9257469ef09e9b4fee5506e4828a783058800b7f
Jermiah Joseph <jermiahjoseph98@gmail.com>
2025-02-26 21:19:41 +0000

    feat: update benchmark script parameters and add seaborn dependency

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 hyperfine \
-  --warmup 2 \
-  --runs 3 \
-  --parameter-list num_cores 1,4,8,12,24,36,48 \
+  --warmup 0 \
+  --runs 10 \
+  --parameter-list num_cores 4,8,12,24,48 \
   --export-csv .imgtools/benchmark.csv \
   --export-json .imgtools/benchmark.json \
   --export-markdown .imgtools/benchmark.md \
-  --command-name "CompareCrawl" \
   ".pixi/envs/dev/bin/python src/imgtools/crawler/crawl2.py -n {num_cores} --force data" \
   ".pixi/envs/dev/bin/imgtools index -j {num_cores} data"
+  # "echo first test with {num_cores} cores; sleep 0.5" \
+  # "echo second test with {num_cores} cores; sleep 0.5"
