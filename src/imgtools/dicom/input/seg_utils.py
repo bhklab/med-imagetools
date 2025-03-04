5952f445e22c15651ec7441f74394b694554e8fe
Jermiah Joseph <jermiahjoseph98@gmail.com>
2025-03-04 14:01:57 +0000

    feat: add custom exceptions for SEG file processing errors

--- src/imgtools/dicom/input/seg_utils.py
+++ src/imgtools/dicom/input/seg_utils.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from typing import TYPE_CHECKING
 
 from pydicom.dataset import FileDataset
@@ -11,7 +13,30 @@ __all__ = [
     "seg_reference_uids",
     "SEGRefSeries",
     "SEGRefSOPs",
+    "MultipleReferencedSeriesError",
+    "NoSegmentationReferencesError",
 ]
+
+
+class MultipleReferencedSeriesError(Exception):
+    """Exception raised when a SEG file contains multiple ReferencedSeriesInstanceUIDs"""
+
+    def __init__(self, count: int) -> None:
+        self.message = (
+            "Multiple ReferencedSeriesInstanceUIDs found in ReferencedSeriesSequence"
+            " This is unexpected and may cause issues. "
+            f"Found {count} ReferencedSeriesInstanceUIDs."
+        )
+        super().__init__(self.message)
+
+
+class NoSegmentationReferencesError(Exception):
+    """Exception raised when a SEG file does not contain any references to source images"""
+
+    def __init__(self, seg_identifier: str | FileDataset) -> None:
+        self.seg_identifier = seg_identifier
+        self.message = f"SEG file: {seg_identifier} does not contain ReferencedSeriesSequence or SourceImageSequence"
+        super().__init__(self.message)
 
 
 class SEGRefSeries(str):
@@ -21,22 +46,24 @@ class SEGRefSeries(str):
 
 
 class SEGRefSOPs(list):
-    """A list representing all the ReferencedSOPInstanceUIDs for a SEG file"""
+    """A list representing *all* the ReferencedSOPInstanceUIDs for a SEG file"""
 
     pass
 
 
 def seg_reference_uids(
     segdcm: DicomInput,
-) -> tuple[SEGRefSeries, SEGRefSOPs] | SEGRefSOPs | None:
+) -> tuple[SEGRefSeries, SEGRefSOPs] | SEGRefSOPs:
     """Get the ReferencedSeriesInstanceUID or ReferencedSOPInstanceUIDs from a SEG file
 
+    Notes
+    -----
     Modern Segmentation objects have a `ReferencedSeriesSequence` attribute
     which contains the `SeriesInstanceUID` of the referenced series and
-    a `ReferencedInstanceSequence` attribute which contains the SOPInstanceUIDs
+    a `ReferencedInstanceSequence` attribute which contains the `SOPInstanceUIDs`
     of the referenced instances.
 
-    Older Segmentation objects have a `SourceImageSequence` attribute which
+    Older Segmentation objects have a `SourceImageSequence` (ISPY2) attribute which
     only contains the `SOPInstanceUIDs` of the referenced instances.
 
     Parameters
@@ -46,24 +73,27 @@ def seg_reference_uids(
 
     Returns
     -------
-    tuple[SEGRefSeries, SEGRefSOPs] | SEGRefSOPs | None
+    tuple[SEGRefSeries, SEGRefSOPs] | SEGRefSOPs
         If the ReferencedSeriesSequence is present, return a tuple of
         the ReferencedSeriesInstanceUID and ReferencedSOPInstanceUIDs.
 
-        OR if the SourceImageSequence is present, return the ReferencedSOPInstanceUIDs.
+        OTHE if the SourceImageSequence is present, return the ReferencedSOPInstanceUIDs.
 
-        OR if neither are present, return None.
+    Raises
+    ------
+    MultipleReferencedSeriesException
+        If the SEG file contains multiple ReferencedSeriesInstanceUIDs
+    NoSegmentationReferencesException
+        If the SEG file does not contain ReferencedSeriesSequence or SourceImageSequence
 
     Examples
     --------
     match seg_reference_uids("/path/to/SEG.dcm"):
-        case None:
-            print("No ReferencedSeriesInstanceUID or ReferencedSOPInstanceUIDs")
-        case ref_series, ref_sop:
+        case case SEGRefSeries(ref_series), SEGRefSOPs(ref_sop):
             print(f"ReferencedSeriesInstanceUID: {ref_series}")
             print(f"ReferencedSOPInstanceUIDs: {ref_sop}")
-        case ref_sop:
-            print(f"ReferencedSOPInstanceUIDs: {ref_sop}")
+        case SEGRefSOPs(ref_sop):
+            print(f"No Referenced SeriesUIDs found. ReferencedSOPInstanceUIDs: {ref_sop}")
     """
 
     seg: FileDataset = load_seg_dcm(segdcm)
@@ -85,16 +115,41 @@ def seg_reference_uids(
         if len(ref_series) == 1:
             return ref_series[0], ref_sop
         else:
-            errmsg = (
-                "Multiple ReferencedSeriesInstanceUIDs found in ReferencedSeriesSequence"
-                " This is unexpected and may cause issues. "
-                f"Found {len(ref_series)} ReferencedSeriesInstanceUIDs."
-            )
-            raise ValueError(errmsg)
+            raise MultipleReferencedSeriesError(len(ref_series))
     elif "SourceImageSequence" in seg:
         ref_sop = SEGRefSOPs(
             [seq.ReferencedSOPInstanceUID for seq in seg.SourceImageSequence]
         )
         return ref_sop
+    else:
+        raise NoSegmentationReferencesError(str(seg.filename))
 
-    return None
+
+if __name__ == "__main__":
+    from pathlib import Path
+
+    from tqdm import tqdm
+
+    from imgtools.dicom.find_dicoms import find_dicoms
+
+    seg_paths = find_dicoms(
+        Path("testdata").expanduser(),
+        recursive=True,
+        check_header=False,
+        extension="dcm",
+        search_input=["SEG_Series"],
+    )
+
+    series_uid_list = []
+    sop_uid_list: list[str] = []
+    for path in tqdm(seg_paths):
+        match seg_reference_uids(path):
+            case SEGRefSeries(ref_series), SEGRefSOPs(ref_sop):
+                series_uid_list.append(ref_series)
+            case SEGRefSOPs(ref_sop):
+                sop_uid_list.extend(ref_sop)
+
+    # print table of # of series_uids and sop_uids
+    print(f"{'series_uid':<20}|{'sop_uid':<20}")
+    print(f"{'-' * 20:<20}{'-' * 20:<20}")
+    print(f"{len(series_uid_list):<20}|{len(sop_uid_list):<20}")
