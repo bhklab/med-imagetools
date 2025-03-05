40cc612230c10dd89cc8bbdd652891a6d90642c1
Jermiah Joseph <44614774+jjjermiah@users.noreply.github.com>
2025-03-05 17:14:00 -0500

    feat: DICOM Parser module (crawl) (#242)
    Introduce a timer decorator for function execution measurement, enhance
    DICOM reference handling, and streamline deprecated module imports.
    Update documentation and tests to reflect these changes.
    
    
    - **New Features**
    - Introduced a new command-line tool for indexing images from a
    specified directory with enhanced options, including output in JSON/CSV
    formats, custom dataset naming, update triggers, and improved help
    guidance when no arguments are provided.
    - Added advanced support for processing DICOM files, enabling efficient
    crawling, metadata extraction, and conversion to a tabular format.
    
    - **Documentation**
    - Improved clarity in documentation for RTSTRUCT file processing,
    detailing alternative return values.
    
    Co-authored-by: skim2257 <hello@sejin.kim>

---
+++ src/imgtools/dicom/crawl/__init__.py
@@ -0,0 +1,26 @@
+from .parse_dicom import ( # noqa
+    MetaAttrDict,
+    SeriesMetaListMap,
+    SeriesMetaMap,
+    SeriesUID,
+    SopSeriesMap,
+    SopUID,
+    SubSeriesID,
+    parse_dicom_dir,
+    parse_one_dicom,
+)
+from .crawler import Crawler # noqa
+
+__all__ = [
+    "Crawler",
+    # parse_dicom
+    "SeriesUID",
+    "SubSeriesID",
+    "SopUID",
+    "MetaAttrDict",
+    "SeriesMetaMap",
+    "SeriesMetaListMap",
+    "SopSeriesMap",
+    "parse_dicom_dir",
+    "parse_one_dicom",
+]
