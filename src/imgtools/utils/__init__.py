eafa6d09f4464b002929e4c317fc1d418cdba8eb
Jermiah Joseph <44614774+jjjermiah@users.noreply.github.com>
2025-03-27 16:59:11 -0400

    chore: update utils  (#281)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **Refactor**
    - Streamlined the module’s public interface by removing outdated
    elements and reorganizing available functionality.
    - Enhanced internal structure and documentation for improved clarity and
    maintainability.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- src/imgtools/utils/__init__.py
+++ src/imgtools/utils/__init__.py
@@ -1,8 +1,3 @@
-from imgtools.dicom.dicom_metadata import (
-    all_modalities_metadata,
-    get_modality_metadata,
-)
-
 from .imageutils import (
     Array3D,
     ImageArrayMetadata,
@@ -16,27 +11,20 @@ from .sanitize_file_name import sanitize_file_name
 from .timer_utils import TimerContext, timed_context, timer
 
 __all__ = [
-    "array_to_image",
-    "image_to_array",
-    "physical_points_to_idxs",
-    "idxs_to_physical_points",
+    # imageutils
     "Array3D",
     "ImageArrayMetadata",
-    "get_modality_metadata",
-    "all_modalities_metadata",
-    "parser",
-    "markdown_report_images",
-    "save_json",
-    "get_identifiers_from_splitted_files",
-    "subfiles",
-    "generate_dataset_json",
-    # Optional import
+    "array_to_image",
+    "idxs_to_physical_points",
+    "image_to_array",
+    "physical_points_to_idxs",
+    # optional_import
     "OptionalImportError",
     "optional_import",
     # sanitize_file_name
     "sanitize_file_name",
-    # timer
-    "timer",
-    "timed_context",
+    # timer_utils
     "TimerContext",
+    "timed_context",
+    "timer",
 ]
