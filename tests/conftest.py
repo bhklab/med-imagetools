f549b160f2f9cce964efb31da1d24e76e3e78280
Jermiah Joseph <jermiahjoseph98@gmail.com>
2025-04-04 16:46:59 +0000

    feat: add test for metadata validation in test_conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 import pytest
 from filelock import FileLock
-
+from enum import Enum
+import os
 import logging
+import pandas as pd
 pytest_logger = logging.getLogger("tests.fixtures")
 pytest_logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter(
@@ -17,11 +19,20 @@ if not DATA_DIR.exists():
     DATA_DIR.mkdir(parents=True, exist_ok=True)
 LOCKFILE = DATA_DIR / ".medimage_testdata.lock"
 
+# TEST_ACCESS_TYPE
+class TestAccessType(str, Enum):
+    PUBLIC = "public"
+    PRIVATE = "private"
+
+TEST_DATASET_TYPE = TestAccessType(
+    os.environ.get("TEST_DATASET_TYPE", "public").lower()
+)
 
 def pytest_sessionstart(session):
     """Called after the Session object has been created and before
     performing collection and entering the run test loop."""
     pytest_logger.info("🔓 Starting pytest session")
+    pytest_logger.info(f"Test dataset type: {TEST_DATASET_TYPE}")
     if Path(LOCKFILE).exists():
         try:
             LOCKFILE.unlink()
@@ -37,12 +48,97 @@ def medimage_test_data() -> Path:
         logger as dataset_logger,
     )
     # prevent multiple workers from downloading simultaneously
+    repo = "bhklab/med-image_test-data"
+    if TEST_DATASET_TYPE == TestAccessType.PRIVATE:
+        repo += "_private"
     with FileLock(LOCKFILE):
         pytest_logger.info("Downloading medimage-testdata...")
-        manager = MedImageTestData()
+        manager = MedImageTestData(repo_name=repo)
         manager.download(dest=DATA_DIR)
     return DATA_DIR
 
+@pytest.fixture(scope="session")
+def medimage_test_data_meta(medimage_test_data) -> list[dict[str, str | Path]]:
+    # try looking for a *-combined_metadata.csv file
+    # in the data directory
+    meta = medimage_test_data / f"{TEST_DATASET_TYPE.upper()}-combined_metadata.csv"
+    metadf = pd.read_csv(meta)
+    missingpaths = []
+    existingpaths = []
+    dataset_dicts = []
+    for row in metadf.itertuples():
+        collection = str(row.Collection).replace(" ", "_")
+        patient_id = str(row.PatientID)
+        modality = str(row.Modality)
+        series_uid = str(row.SeriesInstanceUID)
+        path = medimage_test_data / collection / patient_id / f"{modality}_Series{series_uid[-8:]}"
+        if not path.exists():
+            missingpaths.append(path)
+        else:
+            existingpaths.append(path)
+            dataset_dicts.append({
+                "Collection": collection,
+                "PatientID": patient_id,
+                "Modality": modality,
+                "SeriesInstanceUID": series_uid,
+                "Path": path,
+                "NumInstances": row.ImageCount,
+            })
+    
+    missing_file = medimage_test_data / "missing_paths.txt"
+    if len(missingpaths) > 0:
+        pytest_logger.warning(
+            f"⚠️ The following paths are missing:\n{missingpaths}"
+        )
+        with open(missing_file, "w") as f:
+            for path in missingpaths:
+                f.write(f"{path}\n")
+        raise FileNotFoundError(
+            f"Some paths are missing. Please check {missing_file} "
+            " for more details."
+        )
+        pytest_logger.info(
+            f"✅ The following paths exist:\n{existingpaths}"
+        )
+    else:
+        if missing_file.exists():
+            missing_file.unlink()
+            pytest_logger.info(f"✅ Deleted file: {missing_file}")
+    return dataset_dicts
+
+@pytest.fixture(scope="session")
+def dataset_type() -> str:
+    """Fixture to return the dataset type."""
+    return TEST_DATASET_TYPE.value
+
+@pytest.fixture(scope="session")
+def public_collections() -> list[str]:
+    return  [
+        "4D-Lung",
+        "Adrenal-ACC-Ki67-Seg",
+        "CC-Tumor-Heterogeneity",
+        "ISPY2",
+        "LIDC-IDRI",
+        "Mediastinal-Lymph-Node-SEG",
+        "NSCLC-Radiomics",
+        "NSCLC_Radiogenomics",
+        "Prostate-Anatomical-Edge-Cases",
+        "QIN-PROSTATE-Repeatability",
+        "Soft-tissue-Sarcoma",
+        "Vestibular-Schwannoma-SEG"
+    ]
+
+@pytest.fixture(scope="session")
+def private_collections() -> list[str]:
+    return [
+        "HNSCC",
+        "HNSCC-3DCT-RT",
+        "Head-Neck-PET-CT",
+        "QIN-HEADNECK",
+        "RADCURE",
+        "TCGA-HNSC",
+        "HEAD-NECK-RADIOMICS-HN1"
+    ]
 
 def pytest_sessionfinish(session, exitstatus):
     """Called after whole test run completes, right before returning the exit status."""
