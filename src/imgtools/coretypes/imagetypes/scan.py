6be1212881aed1f29974785467c0f09d9609d93e
Jermiah Joseph <jermiahjoseph98@gmail.com>
2025-04-07 21:22:15 +0000

    feat(scan): implement read_dicom_scan function and add read_dicom_series for DICOM image loading

--- src/imgtools/coretypes/imagetypes/scan.py
+++ src/imgtools/coretypes/imagetypes/scan.py
@@ -1,10 +1,29 @@
-from typing import Dict
+from __future__ import annotations
 
-import SimpleITK as sitk
+from typing import TYPE_CHECKING, Dict
 
 from imgtools.coretypes.imagetypes import MedImage
+from imgtools.io.readers import read_dicom_series
+
+if TYPE_CHECKING:
+    import SimpleITK as sitk
 
 __all__ = ["Scan"]
+
+
+def read_dicom_scan(
+    path: str,
+    series_id: list[str] | None = None,
+    recursive: bool = False,
+    file_names: list[str] | None = None,
+) -> Scan:
+    image = read_dicom_series(
+        path,
+        series_id=series_id,
+        recursive=recursive,
+        file_names=file_names,
+    )
+    return Scan(image, {})
 
 
 class Scan(MedImage):
@@ -17,10 +36,16 @@ class Scan(MedImage):
     def __repr__(self) -> str:  # type: ignore
         # convert metadata and img_stats to string
         # with angulated brackets
-        metadata = "\n\t".join(
-            sorted([f"{k}={v}" for k, v in self.metadata.items()])
-        )
-        img_stats = "\n\t".join(
-            [f"{k}={v}" for k, v in self.img_stats.items()]
-        )
-        return f"Scan<\n\t{metadata}, {img_stats}\n>"
+        retr_str = "Scan<"
+        for k, v in self.metadata.items():
+            retr_str += f"\n\t{k}={v}"
+        for k, v in self.img_stats.items():
+            retr_str += f"\n\t{k}={v}"
+        retr_str += "\n>"
+        return retr_str
+
+
+if __name__ == "__main__":
+    directory = "data/4D-Lung/113_HM10395/CT_Series00173972"
+    scan = read_dicom_scan(directory)
+    print(f"{scan!r}")
