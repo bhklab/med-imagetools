f219f8f81cbd05d76e7d8a2a282b2ae066869d24
Jermiah Joseph <jermiahjoseph98@gmail.com>
2025-04-10 21:11:35 +0000

    Refactor RTStructureSet: add ROI caching, implement get_mask_ndarray and get_label_mask methods, and enhance error handling for empty contours

--- src/imgtools/coretypes/masktypes/structureset.py
+++ src/imgtools/coretypes/masktypes/structureset.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from collections import namedtuple
 from dataclasses import dataclass, field
 from pathlib import Path
 from typing import (
@@ -9,24 +10,25 @@ from typing import (
     List,
 )
 
+import numpy as np
+import SimpleITK as sitk
 from pydicom.dataset import FileDataset
+from skimage.draw import polygon2mask
 
+from imgtools.coretypes.imagetypes import MedImage
+from imgtools.coretypes.masktypes.roi_matching import (
+    ROI_HANDLING,
+    ROIMatcher,
+)
 from imgtools.dicom import DicomInput, load_dicom
 from imgtools.dicom.dicom_metadata import extract_metadata
 from imgtools.exceptions import (
+    ContourPointsAcrossSlicesError,
     MissingROIError,
     ROIContourError,
 )
 from imgtools.loggers import logger
-
-# from imgtools.utils import physical_points_to_idxs
-
-# from .roi_matching import (
-#     ROI_MatchingType,
-#     ROIMatcher,
-#     handle_roi_matching,
-#     match_roi,
-# )
+from imgtools.utils import physical_points_to_idxs
 
 if TYPE_CHECKING:
     from pydicom.dataset import FileDataset
@@ -35,6 +37,9 @@ if TYPE_CHECKING:
 
 class ROIExtractionErrorMsg(str):
     pass
+
+
+ROIMaskMapping = namedtuple("ROIMaskMapping", ["roi_key", "roi_names"])
 
 
 @dataclass
@@ -77,6 +82,14 @@ class RTStructureSet:
     )
     roi_map_errors: dict[str, ROIExtractionErrorMsg] = field(
         repr=False, default_factory=dict, init=False
+    )
+
+    # store a hidden cache to store the numpy arrays
+    # after the first time we access them
+    _roi_cache: dict[str, np.ndarray] = field(
+        repr=False,
+        default_factory=dict,
+        init=False,
     )
 
     @classmethod
@@ -131,7 +144,6 @@ class RTStructureSet:
         rt = cls(
             metadata=metadata,
         )
-        rt.roi_names.extend(metadata["ROINames"])
 
         # Extract ROI contour points for each ROI and
         for roi_index, roi_name in enumerate(metadata["ROINames"]):
@@ -218,6 +230,13 @@ class RTStructureSet:
             )
             raise ROIContourError(msg)
 
+        if len(roi_contour.ContourSequence) == 0:
+            msg = (
+                f"ROI at index {roi_index}, (ReferencedROINumber={roi_index + 1}) "
+                "has an empty 'ContourSequence'."
+            )
+            raise ROIContourError(msg)
+
         # Sometimes the contour could be a POINT or just broken
         # and not have a ContourData
         # Check for ContourData in the first contour
@@ -226,28 +245,6 @@ class RTStructureSet:
             raise ROIContourError(msg)
 
         return roi_contour.ContourSequence
-
-        # # Check for ContourData in each contour
-        # contour_sequence = roi_contour.ContourSequence
-
-        # contour_slices = []
-        # total_num_points = 0
-        # for i, slc in enumerate(contour_sequence):
-        #     if not hasattr(slc, "ContourData"):
-        #         msg = f"Contour {i} in ROI at index {roi_index} is missing 'ContourData'. "
-        #         raise ROIContourError(msg)
-
-        #     roi_points = np.array(slc.ContourData).reshape(-1, 3)
-        # contour_slices.append(ContourSlice(roi_points))
-        # total_num_points += slc.get("NumberOfContourPoints", 0)
-        # cgt = slc.get("ContourGeometricType", None)
-
-        # return ROI(
-        #     name=roi_name,
-        #     ReferencedROINumber=int(roi_contour.ReferencedROINumber),
-        #     num_points=total_num_points,
-        #     slices=contour_slices,
-        # )
 
     def __getitem__(self, idx: str | int | slice) -> Sequence | List[Sequence]:
         """Extend slice based access to the data in the RTStructureSet
@@ -307,10 +304,282 @@ class RTStructureSet:
     def __len__(self) -> int:
         return len(self.roi_names)
 
+    def get_mask_ndarray(
+        self,
+        reference_image: MedImage,
+        roi_name: str,
+        mask_img_size: tuple[int, int, int, int],
+        continuous: bool = True,
+    ) -> np.ndarray:
+        if roi_name in self._roi_cache:
+            return self._roi_cache[roi_name]
 
-# if __name__ == "__main__":
-#     from rich import print
+        try:
+            roi = self.roi_map[roi_name]
+        except KeyError as ke:
+            msg = f"ROI '{roi_name}' not found in RTSTRUCT."
+            raise MissingROIError(msg) from ke
 
+        slices = [np.array(slc.ContourData).reshape(-1, 3) for slc in roi]
+
+        mask_points = physical_points_to_idxs(
+            reference_image, slices, continuous=continuous
+        )
+
+        mask_array_3d = np.zeros(
+            mask_img_size[0:3],
+            dtype=np.uint8,
+        )
+        for contour_num, contour in enumerate(mask_points, start=0):
+            # split the contour into z values and the points
+            uniq_z_vals = list(np.unique(contour[:, 0]))
+            slice_points = contour[:, 1:]
+
+            match uniq_z_vals:
+                # lets make sure that z is 1 unique value
+                case [z] if len(uniq_z_vals) == 1:
+                    z_idx = z
+                case [*z_values]:
+                    raise ContourPointsAcrossSlicesError(
+                        roi_name,
+                        contour_num,
+                        slice_points.shape,
+                        z_values,
+                    )
+                case _:
+                    errmsg = f"Something went wrong with the contour points {uniq_z_vals}."
+                    raise ValueError(errmsg)
+
+            filled_mask_array = polygon2mask(
+                mask_img_size[1:3],
+                slice_points,
+            )
+
+            z_int = int(z_idx)
+
+            # make sure z_int is within the bounds of the mask array
+            assert 0 <= z_int < mask_array_3d.shape[0], (
+                f"z_int {z_int} ({z_idx=}) is out of bounds for mask array "
+                f"with shape {mask_array_3d.shape=}."
+                f" ROI: {roi_name}, "
+                f"ContourNum: {contour_num}, "
+                f"ContourPoints: {slice_points.shape}, "
+                f"ZValues: {uniq_z_vals}"
+            )
+            mask_array_3d[z_int, :, :] = np.logical_or(
+                mask_array_3d[z_int, :, :], filled_mask_array
+            )
+
+        # Store the mask in the cache
+        self._roi_cache[roi_name] = mask_array_3d
+
+        return mask_array_3d
+
+    def get_label_mask(
+        self,
+        reference_image: MedImage,
+        roi_matcher: ROIMatcher,
+        continuous: bool = True,
+    ) -> tuple[sitk.Image, dict[int, ROIMaskMapping]]:
+        matched_rois: list[tuple[str, list[str]]] = roi_matcher.match_rois(
+            self.roi_names
+        )
+        if not matched_rois:
+            errmsg = (
+                f"No matching ROIs found. Available ROIs: {self.roi_names}, "
+            )
+            raise MissingROIError(errmsg)
+
+        logger.debug("Matched ROIs", matched_rois=matched_rois)
+
+        ref_size = reference_image.size
+        mask_img_size: tuple[int, int, int, int] = (
+            ref_size.depth,
+            ref_size.height,
+            ref_size.width,
+            len(matched_rois),
+        )
+
+        mask_array_4d = np.zeros(
+            mask_img_size,
+            dtype=np.uint8,
+        )
+
+        # lets make sure the shape is 4D
+        assert mask_array_4d.ndim == 4
+
+        # we need something to store the mapping
+        # so that we can keep track of what the 3D mask matches to
+        # the original roi name(s)
+        mapping: dict[int, ROIMaskMapping] = {}
+
+        for iroi, (roi_key, matches) in enumerate(matched_rois):
+            logger.debug(
+                f"Processing {roi_key=} ({iroi + 1}/{len(matched_rois)})",
+                matches=matches,
+            )
+            match matches:
+                case [*many_rois]:
+                    # most likely handle type MERGE
+                    for roi_name in many_rois:
+                        mask_3d = self.get_mask_ndarray(
+                            reference_image,
+                            roi_name,
+                            mask_img_size,
+                            continuous=continuous,
+                        )
+                    # here we want to combine the masks in the same 4th dimension
+                    mask_array_4d[:, :, :, iroi] = np.logical_or(
+                        mask_array_4d[:, :, :, iroi], mask_3d
+                    )
+                    mapping[iroi] = ROIMaskMapping(
+                        roi_key=roi_key,
+                        roi_names=many_rois,
+                    )
+
+                case str():
+                    mask_3d = self.get_mask_ndarray(
+                        reference_image,
+                        matched_rois,
+                        mask_img_size,
+                        continuous=continuous,
+                    )
+                    # here each roi is in its own 4th dimension
+                    mask_array_4d[:, :, :, iroi] = mask_3d
+                    mapping[iroi] = ROIMaskMapping(
+                        roi_key=roi_key,
+                        roi_names=[roi_key],
+                    )
+
+        # convert to sitk image
+        mask_image = sitk.GetImageFromArray(mask_array_4d, isVector=True)
+        mask_image.CopyInformation(reference_image)
+
+        assert mask_image.GetPixelIDValue() == 13
+        assert mask_image.GetNumberOfComponentsPerPixel() == len(matched_rois)
+
+        return mask_image, mapping
+
+
+if __name__ == "__main__":  # pragma: no cover
+    from rich import print  # noqa
+    import pandas as pd  # noqa
+    from imgtools.coretypes.imagetypes.scan import read_dicom_scan
+    from tqdm import tqdm  # noqa
+    from imgtools.loggers import tqdm_logging_redirect
+
+    index = Path(".imgtools/data/index.csv")
+
+    index_df = pd.read_csv(index)
+
+    # remove dupicate rows
+    index_df = index_df.drop_duplicates(subset=["SeriesInstanceUID"])
+
+    index_df = index_df.set_index("SeriesInstanceUID", drop=False)
+    rtstructs = index_df.query("Modality == 'RTSTRUCT'")
+
+    results = []
+    failures = []
+    failed = [
+        "1.3.6.1.4.1.14519.5.2.1.262731039041525300359366945100409730057",
+        "1.3.6.1.4.1.14519.5.2.1.145604855576133072144485190249196760430",
+        "1.3.6.1.4.1.14519.5.2.1.161876574066675833575211443252309034265",
+        "1.3.6.1.4.1.14519.5.2.1.303606626100058921970651747217776792853",
+        "1.3.6.1.4.1.14519.5.2.1.239064923357616718786196407609276882856",
+        "1.3.6.1.4.1.14519.5.2.1.75164064738538288645257513911612519591",
+        "1.3.6.1.4.1.14519.5.2.1.100364245183930759730290495595573094436",
+    ]
+    with tqdm_logging_redirect():
+        for _, row in tqdm(
+            rtstructs.iterrows(),
+            total=len(rtstructs),
+            desc="Processing RTSTRUCTs",
+        ):
+            if row["SeriesInstanceUID"] not in failed:
+                continue
+
+            refrow = index_df.loc[str(row.loc["ReferencedSeriesUID"])]
+            if refrow is None:
+                continue
+
+            ref_image = read_dicom_scan(
+                path=str(refrow.loc["folder"]),
+                series_id=str(refrow.loc["SeriesInstanceUID"]),
+            )
+            rtstruct = RTStructureSet.from_dicom(row["folder"])
+
+            for strategy in [
+                ROI_HANDLING.MERGE,
+                # ROI_HANDLING.SEPARATE,
+                # ROI_HANDLING.KEEP_FIRST,
+            ]:
+                matcher = ROIMatcher(
+                    # https://www.aapm.org/pubs/reports/RPT_263.pdf align later
+                    roi_map={
+                        "GTV": [r"gtv.*", r"g.*tv.*"],
+                        "CTV": [r"ctv.*"],
+                        "PTV": [r"PTV.*", r"total PTV", r"uni.*"],
+                        "Tumor": [r"tumor.*", "gtv.*", "oar.*"],
+                        "Brain": [r"brain.*"],
+                        "Prostate": [r"prostate.*"],
+                        "Femur": [r"femur.*"],
+                        "Bladder": [r"bladder.*"],
+                        "Rectum": [r"rectum.*"],
+                        "Lung": [r"lung.*"],
+                        "Heart": [r"heart.*"],
+                        "Liver": [r"liver.*"],
+                        "Kidney": [r"kidney.*"],
+                        "Cochlea": [r"cochlea.*"],
+                        # ??? ['Ut-MRT2-Sag-1', 'Ut-MRT2-Sag-1'] 
+                        "Uterus": [r"uterus.*", "ut.*"], 
+                    },
+                    handling_strategy=strategy,
+                    ignore_case=True,  # important for matching lowercase labels
+                )
+                try:
+                    mask_image, mapping = rtstruct.get_label_mask(
+                        reference_image=ref_image,
+                        roi_matcher=matcher,
+                        continuous=False,
+                    )
+                except MissingROIError:
+                    failures.append(
+                        (
+                            row["folder"],
+                            row["SeriesInstanceUID"],
+                            strategy,
+                            "Missing ROI",
+                            rtstruct.roi_names,
+                        )
+                    )
+                    continue
+                except Exception as e:
+                    logger.error(
+                        f"Failed to get label mask for {row['folder']}",
+                        error=e,
+                    )
+                    failures.append(
+                        (
+                            row["folder"],
+                            row["SeriesInstanceUID"],
+                            strategy,
+                            str(e),
+                            rtstruct.roi_names,
+                        )
+                    )
+                    raise
+                    continue
+
+                results.append(
+                    (
+                        mask_image,
+                        mapping,
+                        strategy,
+                        ref_image,
+                    )
+                )
+
+# p = Path("data/HNSCC/HNSCC-01-0176/RTSTRUCT_Series72843515/00000001.dcm")
 #     p = Path("data/HNSCC/HNSCC-01-0176/RTSTRUCT_Series72843515/00000001.dcm")
 
 #     # Read entire file
