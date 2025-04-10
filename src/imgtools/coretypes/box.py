2f481610a51ae8bc8dc7bb698d753acb56847788
Jermiah Joseph <jermiahjoseph98@gmail.com>
2025-04-10 21:26:04 +0000

    Refactor RegionBox cropping method to use array slicing and update tests to verify pixel physical indices

--- src/imgtools/coretypes/box.py
+++ src/imgtools/coretypes/box.py
@@ -348,7 +348,11 @@ class RegionBox:
         """
         try:
             self.check_out_of_bounds_coordinates(image)
-            cropped_image = sitk.RegionOfInterest(image, self.size, self.min)
+            cropped_image = image[
+                self.min.x : self.max.x,
+                self.min.y : self.max.y,
+                self.min.z : self.max.z,
+            ]
         except Exception as e:
             msg = f"Error cropping image to the box: {e}"
             logger.exception(msg)
