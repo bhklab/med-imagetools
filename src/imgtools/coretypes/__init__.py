f863097c6f0b2bae855e475ea0e7fdb1b37c5316
Jermiah Joseph <jermiahjoseph98@gmail.com>
2025-04-11 19:55:39 +0000

    chore: format

--- src/imgtools/coretypes/__init__.py
+++ src/imgtools/coretypes/__init__.py
@@ -1,5 +1,5 @@
-from .box import BoxPadMethod, RegionBox
 from .base_medimage import MedImage
+from .box import BoxPadMethod, RegionBox
 from .spatial_types.coord_types import Coordinate3D, Size3D, Spacing3D
 
 __all__ = ["MedImage", "Coordinate3D", "Size3D", "Spacing3D", "RegionBox", "BoxPadMethod"]
