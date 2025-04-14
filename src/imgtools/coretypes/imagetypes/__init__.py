b44fd34d2679bec8654b7b1c69e04f6eb8b2903b
Jermiah Joseph <jermiahjoseph98@gmail.com>
2025-04-14 13:47:04 +0000

    fix: remove MedImage from __all__ in imagetypes module

--- src/imgtools/coretypes/imagetypes/__init__.py
+++ src/imgtools/coretypes/imagetypes/__init__.py
@@ -1,6 +1,5 @@
-from .med_image import MedImage  # noqa
 from .dose import Dose
 from .pet import PET
 from .scan import Scan
 
-__all__ = ["MedImage", "Dose", "PET", "Scan"]
+__all__ = ["Dose", "PET", "Scan"]
