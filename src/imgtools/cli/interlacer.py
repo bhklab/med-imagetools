c4128ae857b1c4045f7e302e9524a7fbc8057261
Declan Korda <79344667+DeclanKorda@users.noreply.github.com>
2025-05-14 13:36:30 -0400

    352 improve imgtools interlacer (#360)
    updated the Interlacer CLI logic so that users can now input a directory
    instead of an index file. The function will then search for an index
    file and if none exists, then it will crawl the directory and produce an
    index file before creating the tree.
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Enhanced the CLI command to accept both files and directories as
    input.
    - Added options to specify the number of parallel jobs and to force
    overwrite of existing files.
    - **Bug Fixes**
    - Improved handling of directories and missing index files with
    automated crawling.
    - **Documentation**
    	- Updated help text to reflect new input options and behaviors.
    - **Tests**
    - Introduced integration tests to verify the CLI command works correctly
    with various datasets.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    ---------
    
    Co-authored-by: Declan Michael Korda <DKORDA@uwo.ca>
    Co-authored-by: Jermiah Joseph <jermiahjoseph98@gmail.com>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/imgtools/cli/interlacer.py
+++ src/imgtools/cli/interlacer.py
@@ -1,15 +1,34 @@
 import click
+import os
 from pathlib import Path
+from imgtools.loggers import logger
+
+cpu_count: int | None = os.cpu_count()
+DEFAULT_WORKERS: int = cpu_count - 2 if cpu_count is not None else 1
 
 @click.command(no_args_is_help=True)
 @click.argument(
-    "index_file",
+    "path",
     type=click.Path(
-        exists=True, path_type=Path, dir_okay=False, file_okay=True
+        exists=True, path_type=Path, dir_okay=True, file_okay=True
     ),
 )
+@click.option(
+    "--n-jobs",
+    type=int,
+    default=DEFAULT_WORKERS,
+    help="Number of jobs to use for parallel processing.",
+)
+@click.option(
+    "--force",
+    is_flag=True,
+    default=False,
+    help="Force overwrite existing files.",
+)
 @click.help_option("--help", "-h")
-def interlacer(index_file: Path) -> None:
+def interlacer(path: Path,
+               n_jobs: int,
+               force: bool) -> None:
     """Visualize DICOM series relationships after indexing.
 
     This command will print the tree hierarchy of DICOM series relationships
@@ -18,7 +37,11 @@ def interlacer(index_file: Path) -> None:
     Only shows supported modalities.
 
     \b
-    `interlacer INDEX_FILE will print the series tree for the given index file.
+    `interlacer PATH will print the series tree for the given index file / directory.
+
+    If PATH is a path to an index.csv file, then the tree will be constructed using that index file.
+    If instead PATH is a path to a directory, if the an index file for the directory exists it will be used to contruct the tree, 
+    otherwise the directory will be crawled first.
 
     \b
     The index file should be a CSV file with the following columns:
@@ -33,5 +56,26 @@ def interlacer(index_file: Path) -> None:
     Visit https://bhklab.github.io/med-imagetools/ for more information.
     """
     from imgtools.dicom.interlacer import Interlacer
-    interlacer = Interlacer(index_file)
+    if (path.is_file() and force):
+        logger.warning(f"force requires a directory as input. {path} will be used as the index.")
+    
+    if (path.is_file() and n_jobs > 1):
+        logger.warning(f"n_jobs requires a directory as input. {path} will be used as the index.")
+
+    elif (path.is_dir()):
+        index_path = path.parent / ".imgtools" / path.name / "index.csv"
+        if (force or not index_path.exists()):
+            from imgtools.dicom.crawl import Crawler
+            crawler = Crawler(
+                dicom_dir=path,
+                n_jobs=n_jobs,
+                force=force,
+            )
+            crawler.crawl()
+            logger.info("Crawling completed.")
+            logger.info("Crawl results saved to %s", crawler.output_dir)
+        path = index_path
+
+
+    interlacer = Interlacer(path)
     interlacer.print_tree(None)
